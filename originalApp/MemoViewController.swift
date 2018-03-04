f46c6422c7836f5bc0488c68f74e4d9e9f132bbd
釜野由里佳 <yrkmn19@gmail.com>
2018-03-04 20:41:02 +0900

    ▶︎メモ機能　追加・消去・編集 ▶︎メイン画面に戻るボタン起動

--- originalApp/MemoViewController.swift
+++ originalApp/MemoViewController.swift
@@ -8,18 +8,82 @@
 
 import UIKit
 
-class MemoViewController: UIViewController {
+class MemoViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var titleTextField: UITextField!
+    
+    var todoArray: [String] = []
+    var recieveValue: String!
+    
+    let saveData: UserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //まずsaveDataの中にtodoarrayがあるか確認してなかったら、todoArrayを作成。あったら、todoArrayをsaveDataから取り出す。
+        if saveData.object(forKey: "todos") == nil {
+            //return
+        }else{
+            todoArray = saveData.object(forKey: "todos") as! [String]
+        }
+        
+        titleTextField.delegate = self
+        titleTextField.text = saveData.object(forKey: "todos") as? String
+        
+        
+        if recieveValue != nil {
+            print(recieveValue)
+            titleTextField.text = recieveValue
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    //#007メモ帳　p.２５
+    @IBAction func saveMemo() {
+        //UserDefaultsに書き込み
+        
+        if recieveValue == nil {
+            todoArray.append(titleTextField.text!)
+            saveData.set(todoArray, forKey: "todos")
+        }else if recieveValue != nil {
+            let index = todoArray.index(of: recieveValue)
+            todoArray.remove(at: index!)
+            todoArray.insert(titleTextField.text!, at: index!)
+            saveData.set(todoArray, forKey: "todos")
+            
+        }
+        
+        if titleTextField.text == nil {
+            //return
+        }
+        
+        //alertを出す
+        let alert: UIAlertController = UIAlertController(title: "保存", message:"メモの保存が完了しました。",
+                                                         preferredStyle: .alert)
+        
+        //OKボタン
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: { action in
+                    //ボタンが押されましいたときの動作
+                    //#007 メモ帳P.31
+                    self.navigationController?.popViewController(animated: true)
+                    print("OKボタンが押されました！")
+            }
+            )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
     
 
     /*
--- originalApp/ViewController.swift
+++ originalApp/ViewController.swift
@@ -8,9 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    //Storyboardで扱うTableViewを宣言
+    @IBOutlet var tableView: UITableView!
+    
+    var todos: [String] = []
+    var recieveValue: Double!
+    var value: String!
+    
+    let userDefaults = UserDefaults.standard
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //テーブルビューのデータソースメソッドはViewControllerクラスに書くよ、という設定
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        if let aaa = userDefaults.object(forKey: "todos") {
+            todos = aaa as! [String]
+        }
+        
+        self.tableView.reloadData() //データをリロードする
+        
+    }
     
     @IBAction func back() {
+        dismiss(animated: true, completion: nil)
         
     }
 
@@ -23,7 +49,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //セルを呼び出す
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("todos")
+        self.value = todos[indexPath.row]
+        performSegue(withIdentifier: "ViewController", sender: self.value)
+        
+        tableView.deselectRow(at:indexPath, animated: true)
+    }
+    
+    //セルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    //セクションの数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //セルの内容を決める。
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let todo = todos[indexPath.row]
+        
+        
+        cell.textLabel?.text = todo
+        
+        return cell
+    }
+    //削除機能
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            userDefaults.set(todos, forKey: "todos")
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let memoviewController: MemoViewController =
+            segue.destination as! MemoViewController
+        memoviewController.recieveValue = sender as? String
+        
+        
+        
+    }
 
 }
 
